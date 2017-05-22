309e66533c9860d4fa6cfe48ab5f86efb0456f84
AppDev1226 <app.dev.1226@gmail.com>
2017-05-22 22:39:09 +0800

    init.

---
+++ Node.js/Lab_Crawler/Lab_Crawler_3.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var request = require("request");
+var cheerio = require("cheerio");
+
+request({
+	url : "http://www.taipower.com.tw/content/news/news02.aspx",
+	method : "GET"
+}, function(err, r, body) {
+	if (err || !body) {
+		return;
+	}
+	fs.writeFileSync("test.txt", body);
+});
+
